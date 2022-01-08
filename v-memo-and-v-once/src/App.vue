760294a27e3f437fb618981e2089c2002280dba5
Matthew Maribojoc <matthewmaribojoc@gmail.com>
2022-01-08 16:16:52 -0500

    v-memo and v-once

---
+++ v-memo-and-v-once/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import VOnceExample from "./components/VOnceExample.vue";
+import VMemoExample from "./components/VMemoExample.vue";
+</script>
+
+<template>
+  <v-once-example />
+  <v-memo-example />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ v-memo-and-v-once/src/components/VOnceExample.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+
+const msg = ref("hello world");
+const show = ref(true);
+</script>
+
+<template>
+  <div>
+    <p v-once v-if="show">
+      {{ msg }}
+    </p>
+    <button @click="show = !show">Toggle</button>
+    <button @click="msg = 'changed message'">Change message</button>
+    <div>
+      Current state:
+      <p>msg: {{ msg }}</p>
+      <p>show: {{ show }}</p>
+    </div>
+  </div>
+</template>
